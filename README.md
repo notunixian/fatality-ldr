c599a1bbaffb25dd399afc768d6c5751d9b3c159
rektwareuser <91929042+rektwareuser@users.noreply.github.com>
2021-11-24 18:32:55 +0200

    ......

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # fatality-ldr
+## **PLEASE MAKE SURE YOU DOWNLOAD LOADER FROM HERE, ALL OTHER SOURCES MAY BE UNTRUSTED.**
+# [You can download the latest loader here](https://github.com/notunixian/fatality-ldr/releases)
+
 loader for .gg/stayfatal - fatality.win crack 2021
 
 credits:
@@ -6,3 +9,6 @@ dalkr - doing everything (cracking, creating this loader, etc)
 
 greetz:
 vmexit, harden, synergy - cope harder, prove this is a rat now LAWL!
+
+# Improving on the code/making the loader better
+You can improve on any part of the loader by [forking the repo](https://docs.github.com/en/get-started/quickstart/fork-a-repo) and [making a pull request with your fixed/improved code.](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-pull-requests)
--- fatalcrack_loader/loader.cpp
+++ fatalcrack_loader/loader.cpp
@@ -12,75 +12,75 @@
 #include "fagality.h"
 #include <random>
 
-/* required */
-
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-DWORD FindProcessId( const std::wstring &processName )
+/* required */
+
+DWORD FindProcessId(const std::wstring& processName)
 {
 	PROCESSENTRY32 processInfo;
-	processInfo.dwSize = sizeof( processInfo );
+	processInfo.dwSize = sizeof(processInfo);
 
-	HANDLE processesSnapshot = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, NULL );
-	if( processesSnapshot == INVALID_HANDLE_VALUE )
+	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (processesSnapshot == INVALID_HANDLE_VALUE)
 		return 0;
 
-	Process32First( processesSnapshot, &processInfo );
-	if( !processName.compare( processInfo.szExeFile ) )
+	Process32First(processesSnapshot, &processInfo);
+	if (!processName.compare(processInfo.szExeFile))
 	{
-		CloseHandle( processesSnapshot );
+		CloseHandle(processesSnapshot);
 		return processInfo.th32ProcessID;
 	}
 
-	while( Process32Next( processesSnapshot, &processInfo ) )
+	while (Process32Next(processesSnapshot, &processInfo))
 	{
-		if( !processName.compare( processInfo.szExeFile ) )
+		if (!processName.compare(processInfo.szExeFile))
 		{
-			CloseHandle( processesSnapshot );
+			CloseHandle(processesSnapshot);
 			return processInfo.th32ProcessID;
 		}
 	}
 
-	CloseHandle( processesSnapshot );
+	CloseHandle(processesSnapshot);
 	return 0;
 }
 
-HANDLE get_csgo_handle( int perms = PROCESS_ALL_ACCESS ) {
-	DWORD pid = FindProcessId( L"csgo.exe" );
+HANDLE get_csgo_handle(int perms = PROCESS_ALL_ACCESS) {
+	DWORD pid = FindProcessId(L"csgo.exe");
 
-	if( !pid )
+	if (!pid)
 		return INVALID_HANDLE_VALUE;
 
-	return OpenProcess( perms, FALSE, pid );
+	return OpenProcess(perms, FALSE, pid);
 }
 
-HANDLE get_steam_handle( ) {
-	DWORD pid = FindProcessId( L"steam.exe" );
+HANDLE get_steam_handle() {
+	DWORD pid = FindProcessId(L"steam.exe");
 
-	if( !pid )
+	if (!pid)
 		return INVALID_HANDLE_VALUE;
 
-	return OpenProcess( PROCESS_ALL_ACCESS, FALSE, pid );
+	return OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 }
 
-bool write_memory_to_file( HANDLE hFile, LONG offset, DWORD size, LPCVOID dataBuffer )
+bool write_memory_to_file(HANDLE hFile, LONG offset, DWORD size, LPCVOID dataBuffer)
 {
 	DWORD lpNumberOfBytesWritten = 0;
 	DWORD retValue = 0;
 	DWORD dwError = 0;
 
-	if( ( hFile != INVALID_HANDLE_VALUE ) && dataBuffer )
+	if ((hFile != INVALID_HANDLE_VALUE) && dataBuffer)
 	{
-		retValue = SetFilePointer( hFile, offset, NULL, FILE_BEGIN );
-		dwError = GetLastError( );
+		retValue = SetFilePointer(hFile, offset, NULL, FILE_BEGIN);
+		dwError = GetLastError();
 
-		if( ( retValue == INVALID_SET_FILE_POINTER ) && ( dwError != NO_ERROR ) )
+		if ((retValue == INVALID_SET_FILE_POINTER) && (dwError != NO_ERROR))
 		{
 			return false;
 		}
 		else
 		{
-			if( WriteFile( hFile, dataBuffer, size, &lpNumberOfBytesWritten, 0 ) )
+			if (WriteFile(hFile, dataBuffer, size, &lpNumberOfBytesWritten, 0))
 			{
 				return true;
 			}
@@ -96,14 +96,14 @@ bool write_memory_to_file( HANDLE hFile, LONG offset, DWORD size, LPCVOID dataBu
 	}
 }
 
-bool write_memory_to_new_file( const CHAR *file, DWORD size, LPCVOID dataBuffer )
+bool write_memory_to_new_file(const CHAR* file, DWORD size, LPCVOID dataBuffer)
 {
-	HANDLE hFile = CreateFileA( file, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, 0, 0 );
+	HANDLE hFile = CreateFileA(file, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, 0, 0);
 
-	if( hFile != INVALID_HANDLE_VALUE )
+	if (hFile != INVALID_HANDLE_VALUE)
 	{
-		bool resultValue = write_memory_to_file( hFile, 0, size, dataBuffer );
-		CloseHandle( hFile );
+		bool resultValue = write_memory_to_file(hFile, 0, size, dataBuffer);
+		CloseHandle(hFile);
 		return resultValue;
 	}
 	else
@@ -129,38 +129,45 @@ void inject_desync()
         )";
 	std::cout << "\n";
 
-	SetConsoleTextAttribute(hConsole, 3);
-
+	printf("[*] waiting for steam\n");
 	while ((steam_handle = get_steam_handle(), steam_handle == INVALID_HANDLE_VALUE))
 		Sleep(1000);
 
-	printf("[+] found steam.exe\n");
+	printf("[+] found steam\n");
 
-	char steam_mod_path[] = "C:/Windows/SysWOW64/fatality_steam_module.dll";
+	char steam_mod_path[] = "C:/Windows/SysWOW64/ftc_steam_module.dll";
 	void* steam_module = VirtualAllocEx(steam_handle, nullptr, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	WriteProcessMemory(steam_handle, steam_module, steam_mod_path, sizeof(steam_mod_path), nullptr);
 	HANDLE steam_thread = CreateRemoteThread(steam_handle, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, steam_module, 0, 0);
 
-	printf("[+] open csgo.exe\n");
+	printf("[*] waiting for steam thread to finish\n");
+	WaitForSingleObject(steam_thread, INFINITE);
+
+	printf("[*] waiting for csgo\n");
 	while ((csgo_handle = get_csgo_handle(), csgo_handle == INVALID_HANDLE_VALUE))
 		Sleep(1000);
 
-	printf("[+] found csgo.exe\n");
+	printf("[+] found csgo\n");
 
-	char csgo1_mod_path[] = "C:/Windows/SysWOW64/fatality_loader.dll";
+	Sleep(1000);
+
+	char csgo1_mod_path[] = "C:/Windows/SysWOW64/ftc_loader.dll";
 	void* csgo1_module = VirtualAllocEx(csgo_handle, nullptr, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	WriteProcessMemory(csgo_handle, csgo1_module, csgo1_mod_path, sizeof(csgo1_mod_path), nullptr);
-	HANDLE csgo_desync_thread = CreateRemoteThread(csgo_handle, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, csgo1_module, 0, 0);
+	HANDLE csgo1_legacy_thread = CreateRemoteThread(csgo_handle, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, csgo1_module, 0, 0);
 
-	Sleep(60000);
+	printf("[*] waiting for loader thread to finish\n");
+	WaitForSingleObject(csgo1_legacy_thread, INFINITE);
 
-	char csgo_mod_path[] = "C:/Windows/SysWOW64/fatality_module.dll";
+	char csgo_mod_path[] = "C:/Windows/SysWOW64/ftc_dependency.dll";
 	void* csgo_module = VirtualAllocEx(csgo_handle, nullptr, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	WriteProcessMemory(csgo_handle, csgo_module, csgo_mod_path, sizeof(csgo_mod_path), nullptr);
 	HANDLE csgo_thread = CreateRemoteThread(csgo_handle, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, csgo_module, 0, 0);
 
-	printf("[+] wait few minutes to it load completely (console will close itself in 5 minutes)\n");
+	printf("[+] mapped dependency\n");
 
+	printf("[+] wait few minutes to it load completely (console will close itself in 5 minutes)\n");
+	
 	Sleep(300000);
 }
 
@@ -179,64 +186,72 @@ void inject_legacy()
 
 	SetConsoleTextAttribute(hConsole, 3);
 
-	printf("[+] open steam.exe\n");
+	printf("[*] waiting for steam\n");
 	while ((steam_handle = get_steam_handle(), steam_handle == INVALID_HANDLE_VALUE))
 		Sleep(1000);
 
-	printf("[+] found steam.exe\n");
+	printf("[+] found steam\n");
 
 	char steam_module_path[] = "C:/Windows/SysWOW64/fatal_legacy_steam_module.dll";
 	void* steam_module = VirtualAllocEx(steam_handle, nullptr, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	WriteProcessMemory(steam_handle, steam_module, steam_module_path, sizeof(steam_module_path), nullptr);
 	HANDLE steam_thread = CreateRemoteThread(steam_handle, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, steam_module, 0, 0);
 
-	printf("[!] open csgo.exe\n");
+	printf("[*] waiting for steam thread to finish\n");
+	WaitForSingleObject(steam_thread, INFINITE);
+
+	printf("[*] waiting for csgo\n");
 	while ((csgo_handle = get_csgo_handle(), csgo_handle == INVALID_HANDLE_VALUE))
 		Sleep(1000);
-	printf("[+] found csgo.exe\n");
 
-	Sleep(60000);
+	printf("[+] found csgo\n");
+
+	Sleep(1000);
 
 	char csgo1_mod_path[] = "C:/Windows/SysWOW64/fatal_legacy.dll";
 	void* csgo1_module = VirtualAllocEx(csgo_handle, nullptr, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	WriteProcessMemory(csgo_handle, csgo1_module, csgo1_mod_path, sizeof(csgo1_mod_path), nullptr);
-	HANDLE csgo_legacy_thread = CreateRemoteThread(csgo_handle, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, csgo1_module, 0, 0);
+	HANDLE csgo1_thread = CreateRemoteThread(csgo_handle, nullptr, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, csgo1_module, 0, 0);
+
+	printf("[*] waiting for loader thread to finish\n");
+	WaitForSingleObject(csgo1_thread, INFINITE);
+
+	printf("[+] mapped dependency\n");
 
 	printf("[+] wait few minutes to it load completely (console will close itself in 5 minutes)\n");
 
 	Sleep(300000);
 }
 
-int main( ) {
-
+int main() {
 	SetConsoleTitleA("discord.gg/stayfatal");
 
 	SetConsoleTextAttribute(hConsole, 4);
 
-	if( csgo_handle != INVALID_HANDLE_VALUE ) {
-		TerminateProcess( csgo_handle, 0 );
-		MessageBoxA( 0, "Please open loader before CS:GO", "", 0 );
+	if (csgo_handle != INVALID_HANDLE_VALUE) {
+		TerminateProcess(csgo_handle, 0);
+		MessageBoxA(0, "Please open loader before CS:GO", "", 0);
 		exit(0);
 	}
 
-	if( steam_handle == INVALID_HANDLE_VALUE ) {
-		MessageBoxA( 0, "Please open Steam before loading", "", 0 );
+	if (steam_handle == INVALID_HANDLE_VALUE) {
+		MessageBoxA(0, "Please open Steam before loading", "", 0);
 		return 1;
 	}
 
-	if( !write_memory_to_new_file( "C:/Windows/SysWOW64/fatality_module.dll", sizeof( fagality_dll ), fagality_dll ) ) {
-		printf( "[-] failed to write to C:/Windows/SysWOW64/fatality_module.dll. (missing admin perms?)\n" );
+	if (!write_memory_to_new_file("C:/Windows/SysWOW64/ftc_dependency.dll", sizeof(fagality_dll), fagality_dll)) {
+		printf("[-] failed to write to C:/Windows/SysWOW64/ftc_dependency.dll. (missing admin perms?)\n");
 		return 1;
 	}
 
 
-	if( !write_memory_to_new_file( "C:/Windows/SysWOW64/fatality_steam_module.dll", sizeof( steam_module ), steam_module ) ) {
-		printf( "[-] failed to write to C:/Windows/SysWOW64/fatality_steam_module.dll. (missing admin perms?)\n" );
+	if (!write_memory_to_new_file("C:/Windows/SysWOW64/ftc_steam_module.dll", sizeof(steam_module), steam_module)) {
+		printf("[-] failed to write to C:/Windows/SysWOW64/ftc_steam_module.dll. (missing admin perms?)\n");
 		return 1;
 	}
 
-	if( !write_memory_to_new_file( "C:/Windows/SysWOW64/fatality_loader.dll", sizeof( rawData ), rawData ) ) {
-		printf( "[-] failed to write to C:/Windows/SysWOW64/fatality_loader.dll. (missing admin perms?)\n" );
+	if (!write_memory_to_new_file("C:/Windows/SysWOW64/ftc_loader.dll", sizeof(rawData), rawData)) {
+		printf("[-] failed to write to C:/Windows/SysWOW64/ftc_loader.dll. (missing admin perms?)\n");
 		return 1;
 	}
 	if (!write_memory_to_new_file("C:/Windows/SysWOW64/fatal_legacy_steam_module.dll", sizeof(legacy_steam_module), legacy_steam_module))
@@ -255,10 +270,10 @@ int main( ) {
 	printf("[1] Inject Desync\n");
 	printf("[2] Inject Legacy\n");
 	printf("[!] Choose option: ");
-	
+
 	int option;
 	std::cin >> option;
-	
+
 	system("cls");
 
 	switch (option)
